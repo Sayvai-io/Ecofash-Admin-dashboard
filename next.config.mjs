4f1cfc66b639fff1f5ffeb5cb3f04eca04d96bf1
Balajisrinath R <11407balaji@gmail.com>
2024-09-23 14:43:47 +0530

    blog backed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['https://oqautjxakguunvkpasdv.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oqautjxakguunvkpasdv.supabase.co',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
