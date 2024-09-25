d0bbe9dbfd60eea3e04e4ed4478df66d27f5c1e9
sayvai <prakash@sayvai.io>
2024-09-25 22:26:09 +0530

    added subapase client

--- src/components/Contact/ContactSection.tsx
+++ src/components/Contact/ContactSection.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { createClient } from '@supabase/supabase-js';
 
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+ 
 const ContactSection=()=>{
+
+    
+
+    
     return(
         <div>
-            
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200">
+                Add Contact
+            </button>
         </div>
     )
 }
